c8e2ae13b7d2cbf5217fe55f7995035b0d029dca
Amey Kurade <69680874+AmeyKuradeAK@users.noreply.github.com>
2023-02-16 00:39:20 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './Mycomponent/Navbar';
+
+
+
+function App() {
+  return (
+    <Navbar/>
+    
+    
+  );
+}
+
+export default App;
