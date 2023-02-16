b62e54edc3a31ff836ba065dc39e18a0d2404c7b
Amey Kurade <69680874+AmeyKuradeAK@users.noreply.github.com>
2023-02-16 22:52:01 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './Mycomponent/Navbar';
 
 
 
+
 function App() {
   return (
+    <>
     <Navbar/>
-    
-    
+    </>
   );
 }
 
